e2ff0b422aad9dc0b8ca7388e9235e6b48dc3c78
Rob Hoelz <rob@hoelz.ro>
2012-02-01 19:44:48 -0600

    initial commit

---
+++ lib/Plack/Middleware/WithBody.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Plack::Middleware::WithBody;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT:  A short description of Plack::Middleware::WithBody
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
